8d5eb67778a5b4bfdbfe6257da4123904de50736
LPX Tecnologia <lpxdesenvolvimento@gmail.com>
2026-07-31 20:09:46 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/clipador-profissional/', // ← Nome do repositório
   server: {
     port: 3000,
     open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
   }
 })
